29ae723e0a840b00ad60d36cfa0a7cde851e15c2
c9s <yoanlin93@gmail.com>
2018-01-08 08:07:57 +0800

    fix iface option name

--- vagrant/install-flannel.sh
+++ vagrant/install-flannel.sh
@@ -1,4 +1,4 @@
 # for kubernetes v1.7+
 # kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 curl -s https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > kube-flannel.yml
-perl -ne 'print; print "$1- --iface enp0s8\n" if /(\s*)- --kube-subnet-mgr/;' kube-flannel.yml | kubectl apply -f -
+perl -ne 'print; print "$1- -iface enp0s8\n" if /(\s*)- --kube-subnet-mgr/;' kube-flannel.yml | kubectl apply -f -
