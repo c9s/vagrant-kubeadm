d14c48aac0a450a07e5335b06ede8f3cdda540c2
c9s <yoanlin93@gmail.com>
2018-01-08 12:22:36 +0800

    improve kubeconfig installer script

--- ubuntu/vagrant/install-kubeconfig.sh
+++ ubuntu/vagrant/install-kubeconfig.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
 mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
 
-USER=ubuntu
-USER_HOME=/home/$USER
-mkdir -p $USER_HOME/.kube
-cp -i /etc/kubernetes/admin.conf $USER_HOME/.kube/config
-chown $(id -u $USER):$(id -g $USER) $USER_HOME/.kube/config
+if [[ -f "/etc/kubernetes/admin.conf" ]]
+then
+    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    chown $(id -u):$(id -g) $HOME/.kube/config
+    USER=ubuntu
+    USER_HOME=/home/$USER
+    mkdir -p $USER_HOME/.kube
+    cp -i /etc/kubernetes/admin.conf $USER_HOME/.kube/config
+    chown $(id -u $USER):$(id -g $USER) $USER_HOME/.kube/config
+    cp /etc/kubernetes/admin.conf /vagrant/
+else
+    cp -i /vagrant/admin.conf $HOME/.kube/config
+fi
--- ubuntu/vagrant/setup-hosts.sh
+++ ubuntu/vagrant/setup-hosts.sh
@@ -3,3 +3,6 @@ set -e
 IFNAME=$1
 ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
 sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
+# remove ubuntu-xenial entry
+sed -e '/^.*ubuntu-xenial.*/d' -i /etc/hosts
